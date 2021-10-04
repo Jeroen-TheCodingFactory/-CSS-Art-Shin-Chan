6b6d116065298c2ec817797f14663e028b6f354d
Jeroen-TheCodingFactory <tcf@jeroen-rijsdijk.nl>
2021-10-04 15:17:45 +0200

    added ogp

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ const audio = document.getElementById("js--audio");
 button.onclick = (event) => {
     if(audio.paused === true){
         audio.play();
+        document.title = "Say hey, Hey! Shin Chan";
         return;
     }
+    document.title = "Shin Chan";
     audio.pause();
 }
